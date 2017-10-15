50bb23301b1ad54c1732e7f8b70c950809b13786
Muriam <d_muriam@inbox.ru>
2017-10-15 21:17:11 +0700

    add proba_return

---
+++ return and pointers/proba_cout_return.cpp
@@ -0,0 +1,27 @@
+//#include <cmath>
+#include <iostream>
+#include <conio.h>
+#define S 8
+
+using namespace std;
+
+int proba();
+
+
+int main()
+{
+	cout << "result " << proba();
+	
+	getch();
+	return 0;	
+}	
+
+
+int proba()
+{
+	int sum;
+
+	sum = S + 7;
+	
+	return sum;		
+}	
