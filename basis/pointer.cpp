f98b9a53453cdddcf4ba138310b8801ce9dbd4e0
Muriam <d_muriam@inbox.ru>
2017-10-16 20:30:32 +0700

    add folder

---
+++ basis/pointer.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+
+int main()
+{
+	int  aValue = 76;
+	int *aPtr = &aValue;
+	
+	
+	cout << aValue << endl;  //значение переменной
+    cout << *aPtr << endl;	 //указатель на значение переменой
+    cout << &aValue << endl; //адрес ячейки, где находится переменная
+	
+	
+	getch();
+	return 0;	
+}
---
+++ basis/sumELArray.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+
+int main()
+{
+	int x[30] = {6,2,8};
+	int sum;
+	 		
+	sum = x[0] + x[1] + x[2]; 	
+	
+	cout << "sum = " << sum << endl;
+	
+	
+	getch();
+	return 0;	
+}
