474c054adc92e3620ab4297d603507cb753bf060
Muriam <d_muriam@inbox.ru>
2017-10-24 21:03:59 +0700

    ready interpolation

--- math/range.cpp
+++ math/range.cpp
@@ -2,38 +2,48 @@
 #include <iostream>
 #include <conio.h>
 #include <iomanip> // для setprecision (сколько знаков после запятой в действительном числе)
+#include <locale>
 #define N 5
-#define H 0.011  //эта константа шаг интерполяционной таблицы
+#define H 0.011
 
 using namespace std;
 
 
 int main()
 {
-	double func, result, c, interp;
-	int x[30]; 
-	int i;
-	double a;
+	setlocale(LC_ALL, "rus");
 	
+	double func, result, c, interpol;
+//	int array_1[30]; лишнее
+//	int array_2[31]; лишнее
+	int j,i;
+	double x[31];
+	double y[31];
+		
 	c = N + 1;
 	
+	for (j=0; j<=30; j++)
+	{		
+		x[j] = c+(double)j*H;  	// значения ф-ции по оси Х
+		y[j] = (pow(c,3) * 2) * sin(x[j]/c); 	// значения ф-ции по оси Y  (  f(x)  )
+		
+		cout << " x = " << fixed << setprecision(4) << x[j] << "\t" 
+		<< "f(x) = " << fixed << setprecision(4) << y[j] << endl;	 		
+	}
+	
+	cout << endl;
 	
 	for (i=0; i<=29; i++)
-	{			
-		a = c+(i*H);
-		func = (pow(c,3) * 2) * sin(result/c); //точное значение функции 
-		result = (c + (i * H)) + ((((i%4) + 1) / 5) * H);   //это x(i) 
-		cout << "func = " << setprecision(7) << func << "\t\t" << "x = " << setprecision(7) << result << endl;  									
-	} 
-	
-	/*
-		for (i=0; i<=30; i++)  // цикл для вычисления приближенного значения
-	{
-		interp = 	
-		cout << "interpolation = " << setprecision(7) << interp << endl;  
+	{		
+		result = (c + (double)i * H) + ((((double)(i%4) + double(1)) / 5) * H);   //x промежуточное
+		func = (pow(c,3) * 2) * sin(result/c);                                   //точное значение функции 
+		interpol = (y[i] + ((result - x[i]) / H) * (y[i+1] - y[i]) );		 //интерполяция
+		
+		cout << "x промежут. " << fixed << setprecision(4) << result << "\t" 
+		<< "интерп " << fixed << setprecision(4) << interpol << "\t\t" 
+		<< "точн. " << fixed << setprecision(4) << func << endl;
 	}
-	*/
-	
+
 	getch();
 	return 0;	
-}	
+}
