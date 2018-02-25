9f53f1e96da9af6621ea177c1d0e58e3bd55b668
Muriam <d_muriam@inbox.ru>
2018-02-25 19:57:07 +0700

    Create practice.cpp

---
+++ math/practice.cpp
@@ -0,0 +1,31 @@
+#include <cmath>
+#include <iostream>
+#include <conio.h>
+#include <locale>
+
+using namespace std; 
+ 
+ 
+ int main()
+{
+ 	setlocale(LC_ALL, "rus");
+ 
+ 	
+ 	int a, b, tran;
+ 	cout << "введите число a  " ; 
+  cin >> hex >> a;
+  cout << "введите число b  "; 
+	cin >> hex >> b; 
+    
+	cout << "\nсумма в шестнадцатеричной = " << hex << a + b << endl;
+	cout << "разность в шестнадцатеричной = " << hex << a - b << endl;
+
+	cout << "\nвведите число в шестнадцатеричной  " ; 
+	cin >> hex >> tran;
+	cout << "результат перевода в десятичную  " ; 
+	cout << dec << tran;
+	
+  
+	getch();
+	return 0;
+}
