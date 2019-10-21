ad81c6c25c4faa67c993a3a51702e38d0fe0b7cf
Maria Duvanova <d_muriam@inbox.ru>
2019-10-21 15:18:08 +0600

    Create OOP_file3_main.cpp

---
+++ OOP_file3_main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <conio.h>
+#include <locale>
+#include "class_description.h"
+
+using namespace std;
+
+
+
+int main() 
+{
+    setlocale(LC_ALL, "rus");
+	
+	
+	
+	
+	  getch();
+	  return 0;
+}
